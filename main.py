2145ef7ade5a141c22a9626136da33db92e881fe
Vibilan <vibilan2793@gmail.com>
2026-09-15 08:33:21 +0530

    ..

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 import streamlit as st
+from groq import Groq
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+client = Groq(
+    api_key=os.getenv("GROQ_API_KEY")
+)
 
 st.title("Smart To-Do List")
 
@@ -7,4 +16,10 @@ st.write("")
 x = st.audio_input("IN")
 
 if x:
-    st.write("Audio ready.")
+    transcription = client.audio.transcriptions.create(
+        file=x,
+        model="whisper-large-v3",
+        temperature=0,
+        response_format="verbose_json"
+    )
+    st.write(transcription.text)
