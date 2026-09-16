9bca0df0ceb67fb79e9bae5777bdab218429f3d5
Vibilan <vibilan2793@gmail.com>
2026-09-16 08:51:37 +0530

    gpt-oss model incorporated

--- main.py
+++ main.py
@@ -23,3 +23,31 @@ if x:
         response_format="verbose_json"
     )
     st.write(transcription.text)
+
+    completion = client.chat.completions.create(
+    model="openai/gpt-oss-120b",
+    messages=[
+      {
+        "role": "system",
+        "content": '''
+        You are a smart AI that converts user's cluttered thoughts into a structured to-do list.
+        The user gives you their requirements and you create tasks based on their requirements. Return the tasks in strictly this order:
+        If you find valid tasks in the input: { "status": "ok", "tasks": ["task1", "task2", "task3", ...] }
+        Else (no valid tasks are found): { "status": "failed", "tasks": [] }
+        '''
+      },
+      {
+        "role": "user",
+        "content": transcription.text
+      },
+    ],
+    temperature=1,
+    max_completion_tokens=2048,
+    top_p=1,
+    reasoning_effort="medium",
+    stream=True,
+    stop=None
+)
+    for chunk in completion:
+        if chunk.choices[0].delta.content:
+            st.write(chunk.choices[0].delta.content)
